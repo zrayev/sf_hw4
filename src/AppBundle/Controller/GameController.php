ffab3b889ef97de937e9629e7e11aa2e0efcef3e
dmitry <zrayev@mail.ru>
2015-12-26 23:16:45 +0200

    add form

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Game;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class GameController extends Controller
 {
@@ -14,6 +18,22 @@ class GameController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $game = new Game();
+
+        $form = $this->createFormBuilder($game)
+            ->add('score', TextType::class)
+            ->add('date',DateType::class)
+            ->add('save',SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//
+//        return $this->redirectToRoute('success');
+//        }
+
+
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
         $this->getDoctrine()->getRepository('AppBundle:Game')->findAllQuery(), /* query NOT result */
@@ -21,9 +41,9 @@ class GameController extends Controller
         10/*limit per page*/
     );
 
-
-             return $this->render('AppBundle:Games:games.html.twig', [
-            'pagination' => $pagination
-             ]);
+         return $this->render('AppBundle:Games:games.html.twig', [
+        'pagination' => $pagination,
+        'form' => $form->createView()
+         ]);
     }
 }
